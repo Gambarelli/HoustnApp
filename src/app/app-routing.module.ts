76e28d7f682567eaf85db089794afb0710585bc6
Jose Alvaro Flores Gambarelli <Floresgambarelli@gmail.com>
2019-08-05 16:28:32 -0600

    adds ng-zorro

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'table', pathMatch: 'full' },
+  { path: 'table', loadChildren: () => import('./table/table.module').then(m => m.TableModule) },
+  { path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
+  { path: 'page-header', loadChildren: () => import('./page-header/page-header.module').then(m => m.PageHeaderModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
