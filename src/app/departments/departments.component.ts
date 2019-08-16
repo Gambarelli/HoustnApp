20373045454cb5951e5f8ef69fab664e780eb019
Jose Alvaro Flores Gambarelli <Floresgambarelli@gmail.com>
2019-08-16 08:25:23 -0600

    adds inputs and table to department

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -9,7 +9,29 @@ import { Observable } from 'rxjs';
 })
 export class DepartmentsComponent implements OnInit {
   loading = true;
-  departments: Observable<any[]>;
+  departments: Observable<any[]>;listOfData = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+
+  time: Date | null = null;
+  defaultOpenValue = new Date(0, 0, 0, 0, 0, 0);
 
   constructor(db: AngularFirestore) {
     this.departments = db.collection('departments').valueChanges()
